a243074201609061d8ac6ae07c6e308b46b93987
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-11-02 07:51:29 -0400

    Prompt written.

--- CPP-PRG-10-07-Name-Arranger/main.cpp
+++ CPP-PRG-10-07-Name-Arranger/main.cpp
@@ -5,11 +5,22 @@
 //  Created by Keith Smith on 11/2/17.
 //  Copyright © 2017 Keith Smith. All rights reserved.
 //
+//  Write a program that asks for the user's first, middle, and last names. The names
+//  should be stored in three different character arrays. The program should then store,
+//  in a fourth array, the name arranged in the following manner: the last name followed
+//  by a comma and a space, followed by the first name and a space, followed by the
+//  middle name. For example, if the user entered "Carol Lynn Smith", it should store
+//  "Smith, Carol Lynn" in the fourth array. Display the contents of the fourth array
+//  on the screen.
+//
+//
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main()
+{
+    
     return 0;
 }
